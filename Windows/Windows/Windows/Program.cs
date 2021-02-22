5b3777dfc8f625c4d4a846700f02af9b70b40a9e
Lucas-HGB <lucas.hgb2@gmail.com>
2021-02-22 12:05:20 -0300

    Python Convert
    Alterado código python de rede e informações do host para C#.

--- Windows/Windows/Windows/Program.cs
+++ Windows/Windows/Windows/Program.cs
@@ -31,6 +31,7 @@ namespace Windows
         static void Main(string[] args)
         {
             Hardware infra = new Hardware();
+            Sistema sys = new Sistema();
             bool exit = false;
             do
             {
@@ -44,10 +45,11 @@ namespace Windows
                         break;
                     case 2:
                         Console.Clear();
+                        sys.rede();
                         break;
                     case 3:
                         Console.Clear();
-                        // Host Info
+                        infra.host();
                         break;
                     case 4:
                         Console.Clear();
@@ -68,11 +70,11 @@ namespace Windows
                         {
                             case 1:
                                 Console.WriteLine("--- Application Logs ---");
-                                infra.readlogs("application");
+                                sys.readlogs("application");
                                 break;
                             case 2:
                                 Console.WriteLine("--- System Logs ---");
-                                infra.readlogs("system");
+                                sys.readlogs("system");
                                 break;
                         }
                         break;
@@ -86,9 +88,50 @@ namespace Windows
         }
     }
 
-    
+
     public class Hardware
     {
+
+        public void host()
+        {
+            ManagementObjectCollection cpu = search_wmi("Win32_Processor");
+            foreach (ManagementObject obj in cpu)
+            {
+
+                Console.WriteLine($"Modelo: {obj["Name"]}");
+                Console.WriteLine($"Nr de Cores: {obj["NumberOfCores"]}");
+                Console.WriteLine($"Nr de Processadores lógicos: {obj["NumberOfLogicalProcessors"]}");
+                Console.WriteLine($"Sistema: {obj["AddressWidth"]} bits");
+                Console.WriteLine($"Load Percentage: {obj["LoadPercentage"]}");
+            }
+            ManagementObjectCollection computer = search_wmi("Win32_ComputerSystem");
+            foreach (ManagementObject obj in computer)
+            {
+                Console.WriteLine($"Memória: {getReadableSize(long.Parse(obj["TotalPhysicalMemory"].ToString()))}");
+                Console.WriteLine($"Hostname: {obj["Domain"]}/{obj["Caption"]}");
+                int func_dominio_int = Int32.Parse(obj["DomainRole"].ToString());
+                String func_dominio = "";
+                if (func_dominio_int == 0) { func_dominio = "Standalone Worksation"; }
+                else if (func_dominio_int == 1) { func_dominio = "Member Worksation"; }
+                else if (func_dominio_int == 2) { func_dominio = "Standalone Server"; }
+                else if (func_dominio_int == 3) { func_dominio = "Member Server"; }
+                else if (func_dominio_int == 4) { func_dominio = "Backup Domain Controller"; }
+                else if (func_dominio_int == 5) { func_dominio = "Primary Domain Controller"; }
+                Console.WriteLine($"Função do Domínio: {func_dominio}");
+                Console.WriteLine($"Modelo: {obj["Model"]}");
+                Console.WriteLine($"Arquitetura: {obj["SystemType"]}");
+                Console.WriteLine($"Usuário: {obj["Username"]}");
+                string command = "(cscript  'C:\\Windows\\System32\\slmgr.vbs' /dli)"; Process processo = new Process();
+                processo.StartInfo.FileName = "powershell.exe";
+                processo.StartInfo.UseShellExecute = false;
+                processo.StartInfo.RedirectStandardOutput = true;
+                processo.StartInfo.Arguments = "/c " + command;
+                processo.Start();
+                string output = processo.StandardOutput.ReadToEnd();
+                processo.WaitForExit();
+                Console.WriteLine(output);
+            }
+        }
 
         public static decimal getPercentage(decimal total, decimal valor)
         {
@@ -110,20 +153,17 @@ namespace Windows
             return string.Format("{0:n1} {1}", adjustedSize, SizeSuffixes[mag]);
         }
 
-        // Gets name of current machine for use in LogReader
-        public string hostname { get { return Dns.GetHostName(); } set { hostname = Dns.GetHostName(); } }
-         
         // Used to query WMI class
-        private ManagementObjectSearcher search_wmi(string phrase)
+        private ManagementObjectCollection search_wmi(string classe)
         {
-            ManagementObjectSearcher searcher = new ManagementObjectSearcher(phrase);
-            return searcher;
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher($"SELECT * FROM {classe}");
+            return searcher.Get();
         }
 
         public void list_disks()
         {
             DriveInfo[] allDrives = DriveInfo.GetDrives();
-            Console.WriteLine("Partição" + String.Concat(Enumerable.Repeat("-", 5)) + "Tipo" + String.Concat(Enumerable.Repeat("-", 6)) + "Formato" + String.Concat(Enumerable.Repeat("-", 10)) +  "Espaço Total" + String.Concat(Enumerable.Repeat("-", 10)) + "Espaço Livre" + String.Concat(Enumerable.Repeat("-", 10)) + "Espaço Usado" + String.Concat(Enumerable.Repeat("-", 10)) + "Integridade");
+            Console.WriteLine("Partição" + String.Concat(Enumerable.Repeat("-", 5)) + "Tipo" + String.Concat(Enumerable.Repeat("-", 6)) + "Formato" + String.Concat(Enumerable.Repeat("-", 10)) + "Espaço Total" + String.Concat(Enumerable.Repeat("-", 10)) + "Espaço Livre" + String.Concat(Enumerable.Repeat("-", 10)) + "Espaço Usado");
             foreach (DriveInfo drive in allDrives)
             {
                 if (drive.IsReady)
@@ -131,36 +171,56 @@ namespace Windows
                     string tipo = "";
                     string usedSpace = getReadableSize(drive.TotalSize - drive.AvailableFreeSpace);
                     // Disk type translation
-                    String health = "";
+
                     if (Convert.ToString(drive.DriveType) == "Fixed") { tipo = "Interno"; } else if (Convert.ToString(drive.DriveType) == "Removable") { tipo = "Externo"; } else if (Convert.ToString(drive.DriveType) == "CDRom") { tipo = "CD/DVD"; } else if (Convert.ToString(drive.DriveType) == "Network") { tipo = " Rede"; }
-                    if (tipo != " Rede")
-                    {
-                        String command = "chkdsk " + drive.Name.Substring(0, 2);
-                        System.Diagnostics.Process processo = new System.Diagnostics.Process();
-                        processo.StartInfo.FileName = "Powershell.exe";
-                        processo.StartInfo.UseShellExecute = false;
-                        processo.StartInfo.RedirectStandardOutput = true;
-                        processo.StartInfo.Arguments = "/c " + command;
-                        processo.StartInfo.Verb = "runas";
-                        processo.Start();
-                        string output = processo.StandardOutput.ReadToEnd();
-                        processo.WaitForExit();
-                        Console.WriteLine(output);
-                        if (output == "Não há problemas")
-                        {
-                            health = "Íntrego";
-                        }
-                        else
-                        {
-                            health = "Não Íntegro";
-                        }
-                    }
-                    decimal percentFree = decimal.Round(getPercentage(drive.TotalSize, drive.TotalSize - drive.TotalFreeSpace));
-                    decimal percentUsed = decimal.Round(100 - percentFree);
-                    Console.WriteLine(String.Concat(Enumerable.Repeat(" ", 5 - Convert.ToString(drive.Name).Length)) + drive.Name + String.Concat(Enumerable.Repeat(" ", 10 - Convert.ToString(drive.Name).Length)) + tipo + String.Concat(Enumerable.Repeat(" ", 13 - Convert.ToString(tipo).Length)) + drive.DriveFormat + String.Concat(Enumerable.Repeat(" ", 17 - Convert.ToString(drive.DriveFormat).Length)) + getReadableSize(drive.TotalSize) + String.Concat(Enumerable.Repeat(" ", 20 - Convert.ToString(getReadableSize(drive.TotalSize)).Length)) + $"{getReadableSize(drive.TotalFreeSpace)} {percentFree}%" + String.Concat(Enumerable.Repeat(" ", 18 - Convert.ToString(getReadableSize(drive.TotalFreeSpace)).Length)) + $"{usedSpace} {percentUsed}%" + String.Concat(Enumerable.Repeat(" ", 22 - (usedSpace.Length + Convert.ToString(percentUsed).Length))));
-                    Console.Write(health);
+                    //String health = "";
+                    //if (tipo != " rede")
+                    //{
+                    //    string command = "chkdsk " + drive.name.substring(0, 2);
+                    //    system.diagnostics.process processo = new system.diagnostics.process();
+                    //    processo.startinfo.filename = "powershell.exe";
+                    //    processo.startinfo.useshellexecute = false;
+                    //    processo.startinfo.redirectstandardoutput = true;
+                    //    processo.startinfo.arguments = "/c " + command;
+                    //    processo.startinfo.verb = "runas";
+                    //    processo.start();
+                    //    string output = processo.standardoutput.readtoend();
+                    //    processo.waitforexit();
+                    //    console.writeline(output);
+                    //    if (output == "não há problemas")
+                    //    {
+                    //        health = "íntrego";
+                    //    }
+                    //    else
+                    //    {
+                    //        health = "não íntegro";
+                    //    }
+                    //}
+                    decimal percentUsed = decimal.Round(getPercentage(drive.TotalSize, drive.TotalSize - drive.TotalFreeSpace));
+                    decimal percentFree = decimal.Round(100 - percentUsed);
+                    Console.WriteLine(String.Concat(Enumerable.Repeat(" ", 5 - Convert.ToString(drive.Name).Length)) + drive.Name + String.Concat(Enumerable.Repeat(" ", 10 - Convert.ToString(drive.Name).Length)) + tipo + String.Concat(Enumerable.Repeat(" ", 13 - Convert.ToString(tipo).Length)) + drive.DriveFormat + String.Concat(Enumerable.Repeat(" ", 17 - Convert.ToString(drive.DriveFormat).Length)) + getReadableSize(drive.TotalSize) + String.Concat(Enumerable.Repeat(" ", 20 - Convert.ToString(getReadableSize(drive.TotalSize)).Length)) + $"{getReadableSize(drive.TotalFreeSpace)} {percentFree}%" + String.Concat(Enumerable.Repeat(" ", 22 - $"{getReadableSize(drive.TotalFreeSpace)} {percentFree}%".Length)) + $"{usedSpace} {percentUsed}%" + String.Concat(Enumerable.Repeat(" ", 18 - $"{usedSpace} {percentUsed}%".Length)));
                 }
             }
+        }
+    }
+
+public class Sistema
+    {
+        // Gets name of current machine for use in LogReader
+        public string hostname { get { return Dns.GetHostName(); } set { hostname = Dns.GetHostName(); } }
+
+        public void rede()
+        {
+            string command = "ipconfig /all";
+            Process processo = new Process();
+            processo.StartInfo.FileName = "powershell.exe";
+            processo.StartInfo.UseShellExecute = false;
+            processo.StartInfo.RedirectStandardOutput = true;
+            processo.StartInfo.Arguments = "/c " + command;
+            processo.Start();
+            string output = processo.StandardOutput.ReadToEnd();
+            processo.WaitForExit();
+            Console.WriteLine(output);
         }
 
         public void readlogs(string logtype)
@@ -169,12 +229,10 @@ namespace Windows
             void readSinglelog(EventLogEntry log)
             {
                 Console.WriteLine("Source: " + log.Source);
-                Console.WriteLine("User: " + log.UserName);
                 Console.WriteLine("Entry Type: " + log.EntryType);
                 Console.WriteLine("Category: " + log.Category);
                 Console.WriteLine("Time Generated: " + log.TimeGenerated);
                 Console.WriteLine("Time Written: " + log.TimeWritten);
-                Console.WriteLine("Container: " + log.Container);
                 Console.WriteLine("Index in Event Log: " + log.Index);
                 Console.WriteLine("Instance ID: " + log.InstanceId);
                 Console.WriteLine(" --- Message ---\n" + log.Message + "\n --- End Message ---");
@@ -221,9 +279,9 @@ namespace Windows
             foreach (EventLogEntry entry in entries)
             {
                 if (!blacklist.Contains(entry.Source) && (!added.Contains(entry.InstanceId)))
-                    // If log not in blacklist and log entry hasn't been added
+                // If log not in blacklist and log entry hasn't been added
                 {
-                    
+
                     Console.WriteLine(count + String.Concat(Enumerable.Repeat(" ", 7 - Convert.ToString(count).Length)) + entry.InstanceId + String.Concat(Enumerable.Repeat(" ", 20 - Convert.ToString(entry.InstanceId).Length)) + entry.Source + String.Concat(Enumerable.Repeat(" ", 22 - Convert.ToString(entry.Source).Length)) + entry.EntryType + String.Concat(Enumerable.Repeat(" ", 18 - Convert.ToString(entry.EntryType).Length)) + entry.TimeGenerated);
                     added.Add(entry.InstanceId);
                     logs.Add(entry);
@@ -235,10 +293,16 @@ namespace Windows
                 Console.WriteLine("\n\nPlease insert log Index or enter any non-numeric value to exit");
                 string idx = Console.ReadLine();
                 Console.WriteLine("\n\n");
-                if (idx.All(char.IsDigit)) { readSinglelog(logs[Convert.ToInt32(idx)]); } // If user passed in Int value to get specific Log info
-                else { break; }
+                try
+                {
+                    if (idx.All(char.IsDigit)) { readSinglelog(logs[Convert.ToInt32(idx)]); } // If user passed in Int value to get specific Log info 
+                    else { break; }
+                } catch (FormatException) { break; }
+
             } while (true); // Loop readSingleLog until user inputs non-numeric value.
         }
-        
     }
+       
+        
+
 }
